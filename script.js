1b8cf08c57b0a6714927060eedb5dcdd05ac605a
ryan <rbulluss2000@gmail.com>
2023-06-23 16:29:17 +0100

    change board design to work towards mvp

--- script.js
+++ script.js
@@ -1,41 +1,78 @@
 // constants
 const mainSection = document.querySelector('main');
 const rootVars = document.querySelector(':root');
-
+let ships = {
+    carrier: 5,
+    battleship: 4,
+    cruise: 3,
+    submarine: 3,
+    destroyer: 2
+}
 
 
 // Variables
-let boardSize = 0;
+let boardSize = 7;
 let userSizeChoice = 7;
-
-
-
-
-
-
-
-// createBoard(userSizeChoice);
-
-
-// Clears all elements from the main game area to allow to switch game display
-function clearMain() {
-    const mainEls = document.querySelectorAll('main > *');
-    mainEls.forEach((el) => el.remove());
-}
+let playerBoard = createBoard(7);
+let cpuBoard = createBoard(7);
+let playerState = [];
+let cpuState = [];
 
 
 // Takes an input (x) and creates a board div with x squared nodes
 // it also sets the boardSize var to use in creating the game state 
+
+
+
+function createShips() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function createBoard(size) {
     boardSize = size * size;
     rootVars.style.setProperty('--board-size', size);
     let newDiv = document.createElement('div');
     newDiv.setAttribute('class', 'board');
-    mainSection.prepend(newDiv);
+    mainSection.append(newDiv);
     for (let i = 0; i < boardSize; i++) {
         let newNode = document.createElement('div');
         newNode.setAttribute('class', 'node');
-        newDiv.prepend(newNode);
+        newDiv.append(newNode);
     }
+    return newDiv;
 }
 
+
+// Clears all elements from the main game area to allow to switch game display
+// function clearMain() {
+    //     const mainEls = document.querySelectorAll('main > *');
+    //     mainEls.forEach((el) => el.remove());
+    // }
