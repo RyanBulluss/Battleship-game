83da4766a06bfb4a4b5a068ce2f6ce259b25b739
ryan <rbulluss2000@gmail.com>
2023-06-23 17:03:19 +0100

    add loop to put unique ids on each node

--- script.js
+++ script.js
@@ -10,30 +10,31 @@ let ships = {
 }
 
 
+
+
 // Variables
+let userSizeChoice = 10;
 let boardSize;
-let userSizeChoice = 7;
 let playerBoard;
 let cpuBoard;
 let playerState;
 let cpuState;
 
 
-// Takes an input (x) and creates a board div with x squared nodes
-// it also sets the boardSize var to use in creating the game state 
+
 
 init();
 
 function init() {
-    playerBoard = createBoard(userSizeChoice);
-    cpuBoard = createBoard(userSizeChoice);
+    playerBoard = createBoard('p', userSizeChoice);
+    cpuBoard = createBoard('c', userSizeChoice);
     playerState = createState(userSizeChoice);
     cpuState = createState(userSizeChoice);
     boardSize = userSizeChoice * userSizeChoice;
 }
 
-function createShips() {
-    
+function renderShips() {
+
 
 }
 
@@ -74,7 +75,11 @@ function createState(size) {
     return arrRow;
 }
 
-function createBoard(size) {
+
+// Takes an input (x) and creates a board div with x squared nodes
+// it also sets the boardSize var to use in creating the game state
+// it gives board class and node class as well as id index with p or c for player or cpu
+function createBoard(char, size) {
     boardSize = size * size;
     rootVars.style.setProperty('--board-size', size);
     let newDiv = document.createElement('div');
@@ -83,6 +88,7 @@ function createBoard(size) {
     for (let i = 0; i < boardSize; i++) {
         let newNode = document.createElement('div');
         newNode.setAttribute('class', 'node');
+        newNode.setAttribute('id', `${char + i}`);
         newDiv.append(newNode);
     }
     return newDiv;
